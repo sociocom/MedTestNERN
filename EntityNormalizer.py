faf466cd0321defafe4d139b39ac12f629f2971e
yuka-04 <data2000analysis@gmail.com>
2025-01-22 17:20:23 +0900

    add test

--- EntityNormalizer.py
+++ EntityNormalizer.py
@@ -7,11 +7,11 @@ class EntityDictionary:
 
     def __init__(self, path, candidate_column, normalization_column):
         if path is None:
-            raise ValueError('Path to dictionary file is not specified.')
+            raise ValueError("Path to dictionary file is not specified.")
         if candidate_column is None:
-            raise ValueError('Candidate column is not specified.')
+            raise ValueError("Candidate column is not specified.")
         if normalization_column is None:
-            raise ValueError('Normalization column is not specified.')
+            raise ValueError("Normalization column is not specified.")
 
         self.df = pd.read_csv(path)
         self.candidate_column = candidate_column
@@ -24,36 +24,55 @@ class EntityDictionary:
         return self.df.iloc[:, self.normalization_column].to_list()
 
     def get_normalized_term(self, term):
-        return self.df[self.df.iloc[:, self.candidate_column] == term].iloc[:, self.normalization_column].item()
+        return (
+            self.df[self.df.iloc[:, self.candidate_column] == term]
+            .iloc[:, self.normalization_column]
+            .item()
+        )
 
 
 class DefaultDiseaseDict(EntityDictionary):
 
     def __init__(self):
-        super().__init__('dictionaries/disease_dict.csv', 0, 2)
+        super().__init__("dictionaries/disease_dict.csv", 0, 2)
 
 
 class DefaultDrugDict(EntityDictionary):
 
     def __init__(self):
-        super().__init__('dictionaries/drug_dict.csv', 0, 2)
+        super().__init__("dictionaries/drug_dict.csv", 0, 2)
+
+
+class DefaultTest(EntityDictionary):
+
+    def __init__(self):
+        super().__init__("dictionaries/test_dict.csv", 0, 2)
 
 
 class EntityNormalizer:
 
-    def __init__(self, database: EntityDictionary, matching_method=fuzz.ratio, matching_threshold=0):
+    def __init__(
+        self,
+        database: EntityDictionary,
+        matching_method=fuzz.ratio,
+        matching_threshold=0,
+    ):
         self.database = database
         self.matching_method = matching_method
         self.matching_threshold = matching_threshold
-        self.candidates = [mojimoji.han_to_zen(x) for x in self.database.get_candidates_list()]
+        self.candidates = [
+            mojimoji.han_to_zen(x) for x in self.database.get_candidates_list()
+        ]
 
     def normalize(self, term):
         term = mojimoji.han_to_zen(term)
-        preferred_candidate = process.extractOne(term, self.candidates, scorer=self.matching_method)
+        preferred_candidate = process.extractOne(
+            term, self.candidates, scorer=self.matching_method
+        )
         score = preferred_candidate[1]
 
         if score > self.matching_threshold:
             ret = self.database.get_normalized_term(preferred_candidate[0])
-            return ('' if pd.isna(ret) else ret), score
+            return ("" if pd.isna(ret) else ret), score
         else:
-            return '', score
+            return "", score
--- predict.py
+++ predict.py
@@ -9,9 +9,19 @@ from tqdm import tqdm
 
 import NER_medNLP as ner
 import utils
-from EntityNormalizer import EntityNormalizer, EntityDictionary, DefaultDiseaseDict, DefaultDrugDict
+from EntityNormalizer import (
+    EntityNormalizer,
+    EntityDictionary,
+    DefaultDiseaseDict,
+    DefaultDrugDict,
+    DefaultTest,
+)
 
-device = torch.device("mps" if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device(
+    "mps"
+    if torch.backends.mps.is_available()
+    else "cuda" if torch.cuda.is_available() else "cpu"
+)
 
 # %% global変数として使う
 dict_key = {}
@@ -22,33 +32,35 @@ def to_xml(data, id_to_tags):
     with open("key_attr.pkl", "rb") as tf:
         key_attr = pickle.load(tf)
 
-    text = data['text']
+    text = data["text"]
     count = 0
-    for i, entities in enumerate(data['entities_predicted']):
+    for i, entities in enumerate(data["entities_predicted"]):
         if entities == "":
             return
-        span = entities['span']
+        span = entities["span"]
         try:
-            type_id = id_to_tags[entities['type_id']].split('_')
+            type_id = id_to_tags[entities["type_id"]].split("_")
         except:
             print("out of rage type_id", entities)
             continue
         tag = type_id[0]
 
         if not type_id[1] == "":
-            attr = ' ' + value_to_key(type_id[1], key_attr) + '=' + '"' + type_id[1] + '"'
+            attr = (
+                " " + value_to_key(type_id[1], key_attr) + "=" + '"' + type_id[1] + '"'
+            )
         else:
             attr = ""
 
-        if 'norm' in entities:
-            attr = attr + ' norm="' + str(entities['norm']) + '"'
+        if "norm" in entities:
+            attr = attr + ' norm="' + str(entities["norm"]) + '"'
 
         add_tag = "<" + str(tag) + str(attr) + ">"
-        text = text[:span[0] + count] + add_tag + text[span[0] + count:]
+        text = text[: span[0] + count] + add_tag + text[span[0] + count :]
         count += len(add_tag)
 
         add_tag = "</" + str(tag) + ">"
-        text = text[:span[1] + count] + add_tag + text[span[1] + count:]
+        text = text[: span[1] + count] + add_tag + text[span[1] + count :]
         count += len(add_tag)
     return text
 
@@ -61,11 +73,9 @@ def predict_entities(model, tokenizer, sentences_list):
     text_entities_set = []
     for dataset in sentences_list:
         text_entities = []
-        for sample in tqdm(dataset, desc='Predict', leave=False):
+        for sample in tqdm(dataset, desc="Predict", leave=False):
             text = sample
-            encoding, spans = tokenizer.encode_plus_untagged(
-                text, return_tensors='pt'
-            )
+            encoding, spans = tokenizer.encode_plus_untagged(text, return_tensors="pt")
             encoding = {k: v.to(device) for k, v in encoding.items()}
 
             with torch.no_grad():
@@ -80,7 +90,9 @@ def predict_entities(model, tokenizer, sentences_list):
 
             # entities_list.append(sample['entities'])
             entities_predicted_list.append(entities_predicted)
-            text_entities.append({'text': text, 'entities_predicted': entities_predicted})
+            text_entities.append(
+                {"text": text, "entities_predicted": entities_predicted}
+            )
         text_entities_set.append(text_entities)
     return text_entities_set
 
@@ -91,7 +103,7 @@ def combine_sentences(text_entities_set, id_to_tags, insert: str):
         document = []
         for t in text_entities:
             document.append(to_xml(t, id_to_tags))
-        documents.append('\n'.join(document))
+        documents.append("\n".join(document))
     return documents
 
 
@@ -107,35 +119,79 @@ def value_to_key(value, key_attr):  # attributeから属性名を取得
 
 
 # %%
-def normalize_entities(text_entities_set, id_to_tags, disease_dict=None, disease_candidate_col=None, disease_normalization_col=None, disease_matching_threshold=None, drug_dict=None,
-                       drug_candidate_col=None, drug_normalization_col=None, drug_matching_threshold=None):
+def normalize_entities(
+    text_entities_set,
+    id_to_tags,
+    disease_dict=None,
+    disease_candidate_col=None,
+    disease_normalization_col=None,
+    disease_matching_threshold=None,
+    drug_dict=None,
+    drug_candidate_col=None,
+    drug_normalization_col=None,
+    drug_matching_threshold=None,
+    test_dict=None,
+    test_candidate_col=None,
+    test_normalization_col=None,
+    test_matching_threshold=None,
+):
     if disease_dict:
-        disease_dict = EntityDictionary(disease_dict, disease_candidate_col, disease_normalization_col)
+        disease_dict = EntityDictionary(
+            disease_dict, disease_candidate_col, disease_normalization_col
+        )
     else:
         disease_dict = DefaultDiseaseDict()
-    disease_normalizer = EntityNormalizer(disease_dict, matching_threshold=disease_matching_threshold)
+    disease_normalizer = EntityNormalizer(
+        disease_dict, matching_threshold=disease_matching_threshold
+    )
 
     if drug_dict:
-        drug_dict = EntityDictionary(drug_dict, drug_candidate_col, drug_normalization_col)
+        drug_dict = EntityDictionary(
+            drug_dict, drug_candidate_col, drug_normalization_col
+        )
     else:
         drug_dict = DefaultDrugDict()
-    drug_normalizer = EntityNormalizer(drug_dict, matching_threshold=drug_matching_threshold)
+    drug_normalizer = EntityNormalizer(
+        drug_dict, matching_threshold=drug_matching_threshold
+    )
 
-    for entry in tqdm(text_entities_set, desc='Normalization', leave=False):
+    if test_dict:
+        test_dict = EntityDictionary(
+            test_dict, test_candidate_col, test_normalization_col
+        )
+    else:
+        test_dict = DefaultTest()
+    test_normalizer = EntityNormalizer(
+        test_dict, matching_threshold=test_matching_threshold
+    )
+
+    for entry in tqdm(text_entities_set, desc="Normalization", leave=False):
         for text_entities in entry:
-            entities = text_entities['entities_predicted']
+            entities = text_entities["entities_predicted"]
             for entity in entities:
-                tag = id_to_tags[entity['type_id']].split('_')[0]
+                tag = id_to_tags[entity["type_id"]].split("_")[0]
 
-                normalizer = drug_normalizer if tag == 'm-key' \
-                    else disease_normalizer if tag == 'd' \
-                    else None
+                normalizer = (
+                    drug_normalizer
+                    if tag == "m-key"
+                    else disease_normalizer if tag == "d" else None
+                )
+                normalizer = (
+                    test_normalizer
+                    if tag == "t-test"
+                    else disease_normalizer if tag == "d" else None
+                )
+                normalizer = (
+                    test_normalizer
+                    if tag == "t-key"
+                    else disease_normalizer if tag == "d" else None
+                )
 
                 if normalizer is None:
                     continue
 
-                normalization, score = normalizer.normalize(entity['name'])
-                entity['norm'] = str(normalization)
+                normalization, score = normalizer.normalize(entity["name"])
+                entity["norm"] = str(normalization)
 
 
 def run(model, input, output=None, normalize=False, **kwargs):
@@ -144,17 +200,23 @@ def run(model, input, output=None, normalize=False, **kwargs):
     len_num_entity_type = len(id_to_tags)
 
     # Load the model and tokenizer
-    classification_model = ner.BertForTokenClassification_pl.from_pretrained_bin(model_path=model, num_labels=2 * len_num_entity_type + 1)
+    classification_model = ner.BertForTokenClassification_pl.from_pretrained_bin(
+        model_path=model, num_labels=2 * len_num_entity_type + 1
+    )
     bert_tc = classification_model.bert_tc.to(device)
 
     tokenizer = ner.NER_tokenizer_BIO.from_pretrained(
-        'tohoku-nlp/bert-base-japanese-whole-word-masking',
-        num_entity_type=len_num_entity_type  # Entityの数を変え忘れないように！
+        "tohoku-nlp/bert-base-japanese-whole-word-masking",
+        num_entity_type=len_num_entity_type,  # Entityの数を変え忘れないように！
     )
 
     # Load input files
-    if (os.path.isdir(input)):
-        files = [os.path.join(input, f) for f in os.listdir(input) if os.path.isfile(os.path.join(input, f))]
+    if os.path.isdir(input):
+        files = [
+            os.path.join(input, f)
+            for f in os.listdir(input)
+            if os.path.isfile(os.path.join(input, f))
+        ]
     else:
         files = [input]
 
@@ -163,7 +225,7 @@ def run(model, input, output=None, normalize=False, **kwargs):
             with open(file) as f:
                 articles_raw = f.read()
 
-            article_norm = unicodedata.normalize('NFKC', articles_raw)
+            article_norm = unicodedata.normalize("NFKC", articles_raw)
 
             sentences_raw = utils.split_sentences(articles_raw)
             sentences_norm = utils.split_sentences(article_norm)
@@ -171,19 +233,19 @@ def run(model, input, output=None, normalize=False, **kwargs):
             text_entities_set = predict_entities(bert_tc, tokenizer, [sentences_norm])
 
             for i, texts_ent in enumerate(text_entities_set[0]):
-                texts_ent['text'] = sentences_raw[i]
+                texts_ent["text"] = sentences_raw[i]
 
             if normalize:
                 normalize_entities(text_entities_set, id_to_tags, **kwargs)
 
-            documents = combine_sentences(text_entities_set, id_to_tags, '\n')
+            documents = combine_sentences(text_entities_set, id_to_tags, "\n")
 
             tqdm.write(f"File: {file}")
             tqdm.write(documents[0])
             tqdm.write("")
 
             if output:
-                with open(file.replace(input, output), 'w') as f:
+                with open(file.replace(input, output), "w") as f:
                     f.write(documents[0])
 
         except Exception as e:
@@ -192,23 +254,96 @@ def run(model, input, output=None, normalize=False, **kwargs):
             tqdm.write("")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Predict entities from text')
-    parser.add_argument('-m', '--model', type=str, default='pytorch_model.bin', help='Path to model checkpoint')
-    parser.add_argument('-i', '--input', type=str, default='text.txt', help='Path to text file or directory')
-    parser.add_argument('-o', '--output', type=str, default=None, help='Path to output file or directory')
-    parser.add_argument('-n', '--normalize', action=argparse.BooleanOptionalAction, help='Enable entity normalization', default=False)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Predict entities from text")
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        default="pytorch_model.bin",
+        help="Path to model checkpoint",
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        type=str,
+        default="text.txt",
+        help="Path to text file or directory",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        default=None,
+        help="Path to output file or directory",
+    )
+    parser.add_argument(
+        "-n",
+        "--normalize",
+        action=argparse.BooleanOptionalAction,
+        help="Enable entity normalization",
+        default=False,
+    )
 
     # Dictionary override arguments
-    parser.add_argument("--drug-dict", help="File path for overriding the default drug dictionary")
-    parser.add_argument("--drug-candidate-col", type=int, help="Column name for drug candidates in the CSV file (required if --drug-dict is specified)")
-    parser.add_argument("--drug-normalization-col", type=int, help="Column name for drug normalization in the CSV file (required if --drug-dict is specified")
-    parser.add_argument('--disease-matching-threshold', type=int, default=50, help='Matching threshold for disease dictionary')
+    parser.add_argument(
+        "--drug-dict", help="File path for overriding the default drug dictionary"
+    )
+    parser.add_argument(
+        "--drug-candidate-col",
+        type=int,
+        help="Column name for drug candidates in the CSV file (required if --drug-dict is specified)",
+    )
+    parser.add_argument(
+        "--drug-normalization-col",
+        type=int,
+        help="Column name for drug normalization in the CSV file (required if --drug-dict is specified",
+    )
+    parser.add_argument(
+        "--disease-matching-threshold",
+        type=int,
+        default=50,
+        help="Matching threshold for disease dictionary",
+    )
 
-    parser.add_argument("--disease-dict", help="File path for overriding the default disease dictionary")
-    parser.add_argument("--disease-candidate-col", type=int, help="Column name for disease candidates in the CSV file (required if --disease-dict is specified)")
-    parser.add_argument("--disease-normalization-col", type=int, help="Column name for disease normalization in the CSV file (required if --disease-dict is specified)")
-    parser.add_argument('--drug-matching-threshold', type=int, default=50, help='Matching threshold for drug dictionary')
+    parser.add_argument(
+        "--disease-dict", help="File path for overriding the default disease dictionary"
+    )
+    parser.add_argument(
+        "--disease-candidate-col",
+        type=int,
+        help="Column name for disease candidates in the CSV file (required if --disease-dict is specified)",
+    )
+    parser.add_argument(
+        "--disease-normalization-col",
+        type=int,
+        help="Column name for disease normalization in the CSV file (required if --disease-dict is specified)",
+    )
+    parser.add_argument(
+        "--drug-matching-threshold",
+        type=int,
+        default=50,
+        help="Matching threshold for drug dictionary",
+    )
+    parser.add_argument(
+        "--test-dict", help="File path for overriding the default test dictionary"
+    )
+    parser.add_argument(
+        "--test-candidate-col",
+        type=int,
+        help="Column name for test candidates in the CSV file (required if --test-dict is specified)",
+    )
+    parser.add_argument(
+        "--test-normalization-col",
+        type=int,
+        help="Column name for test normalization in the CSV file (required if --test-dict is specified)",
+    )
+    parser.add_argument(
+        "--test-matching-threshold",
+        type=int,
+        default=50,
+        help="Matching threshold for test dictionary",
+    )
     args = parser.parse_args()
 
     argument_dict = vars(args)
